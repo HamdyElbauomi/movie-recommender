ab11a2810d98adf52ce202d34406c689c334587c
Hamdy Elbauomi <elbauomyhamdy@gmail.com>
2025-05-04 22:23:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,42 +2,18 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
-import os
-import gdown
+import base64
 
-def download_file_from_drive(file_id, destination):
-    url = f'https://drive.google.com/uc?id={file_id}'
-    gdown.download(url, output=destination, quiet=False)
 
-def is_valid_pickle_file(file_path):
-    try:
-        with open(file_path, "rb") as f:
-            pickle.load(f)
-        return True
-    except Exception:
-        return False
 
-# Download the similarity.pkl file from Google Drive using gdown
-def ensure_similarity_file(file_id, local_path):
-    if not os.path.exists(local_path) or not is_valid_pickle_file(local_path):
-        download_file_from_drive(file_id, local_path)
-        if not is_valid_pickle_file(local_path):
-            st.error("The downloaded file is not a valid pickle. Please check the file ID or permissions.")
-            st.stop()
-
-file_id = "16qftb-hYK9qdnc2ZOPSP8awthhb_vyNS"
-similarity_file = "similarity.pkl"
-ensure_similarity_file(file_id, similarity_file)
-similarity = pickle.load(open(similarity_file, "rb"))
-
-# Set app background
-
-def set_background(image_url):
+def set_background(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode()
     st.markdown(
         f"""
         <style>
         .stApp {{
-            background-image: url(\"{image_url}\");
+            background-image: url("data:image/avif;base64,{encoded_string}");
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
@@ -47,40 +23,62 @@ def set_background(image_url):
         unsafe_allow_html=True
     )
 
-set_background("https://4kwallpapers.com/images/walls/thumbs_3t/4845.jpg")
+# Use the local image
+set_background("D:\Desktop\R_move_project\zack-snyders-justice-league-2021-movies-superman-batman-3840x2160-4845.jpg")
 
-# Fetch poster helper
+ 
+
+
 def fetch_poster(movie_id):
-    response = requests.get(
-        f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=69f5cea05ad0d7ad5c34a00fa93b3462&language=en-US'
-    )
-    data = response.json()
-    return f"https://image.tmdb.org/t/p/w500/{data.get('poster_path', '')}"
+    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=69f5cea05ad0d7ad5c34a00fa93b3462&language=en-US')
+    response_json = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + response_json['poster_path']
 
-# Recommendation logic
 def recommend(movie_name):
-    idx = movies[movies['title'] == movie_name].index[0]
-    distances = similarity[idx]
-    pairs = sorted(list(enumerate(distances)), key=lambda x: x[1], reverse=True)[1:6]
+    movie_index = movies[movies['title'] == movie_name].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    names, posters = [], []
-    for i, _ in pairs:
-        movie_id = movies.iloc[i]['movie_id']
-        names.append(movies.iloc[i]['title'])
-        posters.append(fetch_poster(movie_id))
-    return names, posters
+    recommended_movies = []
+    recommended_movies_posters = []
 
-# Load movies list
-movies_dict = pickle.load(open('movies.pkl', 'rb'))
-movies = pd.DataFrame(movies_dict)
+    for i in movie_list:
+        index = i[0]
+        movie_id = index
+        # fetch poster from API
+        recommended_movies.append(movies.iloc[index]['title'])
+        recommended_movies_posters.append(fetch_poster(movies.iloc[index]['movie_id']))
 
-# Streamlit UI
+    return recommended_movies, recommended_movies_posters
+
+
+moveies_dict = pickle.load(open('movies.pkl', 'rb'))
+movies = pd.DataFrame(moveies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
 st.title('Movie Recommender System')
-selected = st.selectbox('Type or select a movie:', movies['title'].values)
+selected_movie_name = st.selectbox(
+    'Type or select a movie from the dropdown',
+    movies['title'].values)
+
 if st.button('Show Recommendation'):
-    names, posters = recommend(selected)
-    cols = st.columns(5)
-    for col, name, poster in zip(cols, names, posters):
-        with col:
-            st.text(name)
-            st.image(poster)  
+    names, posters =  recommend(selected_movie_name)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+    
